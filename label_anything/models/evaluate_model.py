83a7d58746be4bffbde65078b8fb5d4232db317d
nicolafan <48762613+nicolafan@users.noreply.github.com>
2023-10-02 17:48:41 +0200

    Load Intersection over Union metric

---
+++ label_anything/models/evaluate_model.py
@@ -0,0 +1,30 @@
+import torch
+
+
+def iou(pred: torch.Tensor, target: torch.Tensor):
+    """Compute the intersection over union (IoU) per class.
+
+    Args:
+        pred (torch.Tensor): A tensor of shape (N, C, H, W) representing the predicted masks.
+        target (torch.Tensor): A tensor of shape (N, H, W) representing the true masks.
+
+    Returns:
+        ious (list): A list of length C containing the IoU for each class.
+    """
+    ious = []
+    pred = torch.argmax(pred, dim=1).view(-1)
+    target = target.view(-1)
+    for cls in range(pred.shape[1]):
+        pred_inds = pred == cls
+        target_inds = target == cls
+        intersection = (pred_inds[target_inds]).long().sum().item()
+        union = (
+            pred_inds.long().sum().item()
+            + target_inds.long().sum().item()
+            - intersection
+        )
+        if union == 0:
+            ious.append(float("nan"))
+        else:
+            ious.append(intersection / union)
+    return ious
