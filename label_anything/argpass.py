5900c41f87d612fc6a87ad141979baf89b8a002b
colonnamanu98 <emanuele@localhost.localdomain>
2023-10-03 12:42:27 +0200

    fix argparse

--- label_anything/argpass.py
+++ label_anything/argpass.py
@@ -1,8 +1,40 @@
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--experiment', type=str, required=True, default='Exp')
-parser.add_argument('--file', type=str, help='Set the config file')
-parser.add_argument('--dir', default=None, help='Set the local tracking directory')
+from utils.utils import load_yaml
+import yaml
+import re
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Load the YAML configuration
+    config_list = load_yaml('parameters.yaml')
+
+    # Convert the list of dictionaries into a single dictionary
+    config = {}
+    for item in config_list:
+        config.update(item)
+
+     # Iterate through the config and add arguments to the parser
+    for key, value in config.items():
+        if isinstance(value, dict) and 'help' in value and 'value' in value:
+            name = value['help']
+            default_value = value['value']
+        else:
+            name = key.replace("_", " ").capitalize()
+            default_value = value
+
+        if isinstance(default_value, list):
+            # Handle lists by specifying the type of the first element
+            parser.add_argument(
+                f"--{key}", type=type(default_value[0]), nargs="+", default=default_value, help=name)
+        elif isinstance(default_value, dict):
+            # Handle dictionaries by converting them to strings
+            yaml_string = yaml.dump(default_value)
+            parser.add_argument(f"--{key}", type=str,
+                                default=yaml_string, help=name)
+        else:
+            parser.add_argument(
+                f"--{key}", type=type(default_value), default=default_value, help=name)
+
+    return parser.parse_args()
