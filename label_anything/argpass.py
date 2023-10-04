99b68209ac1070cd742da8942acac8509209d1f5
colonnamanu98 <emanuele@localhost.localdomain>
2023-10-04 17:15:58 +0200

    fix argparse

--- label_anything/argpass.py
+++ label_anything/argpass.py
@@ -3,37 +3,109 @@ from utils.utils import load_yaml
 import yaml
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
+import argparse
+import yaml
 
-    # Load the YAML configuration
-    config_list = load_yaml('parameters.yaml')
 
-    # Convert the list of dictionaries into a single dictionary
-    config = {}
-    for item in config_list:
-        config.update(item)
+# Helper function to determine argument type
+def get_arg_type(value):
+    if isinstance(value, int):
+        return int
+    elif isinstance(value, float):
+        return float
+    elif isinstance(value, str):
+        return str
+    elif isinstance(value, bool):
+        return bool
+    else:
+        return type(value[0]) if isinstance(value, list) and value else str
 
-     # Iterate through the config and add arguments to the parser
-    for key, value in config.items():
-        if isinstance(value, dict) and 'help' in value and 'value' in value:
-            name = value['help']
-            default_value = value['value']
+
+# Helper function to parse the 'parameters' section
+def parse_parameters(parameters):
+    parsed_params = {}
+    for key, value in parameters.items():
+        if isinstance(value, dict) and "help" in value and "value" in value:
+            name = value["help"]
+            default_value = value["value"]
         else:
             name = key.replace("_", " ").capitalize()
             default_value = value
 
+        arg_type = get_arg_type(default_value)
+
         if isinstance(default_value, list):
             # Handle lists by specifying the type of the first element
-            parser.add_argument(
-                f"--{key}", type=type(default_value[0]), nargs="+", default=default_value, help=name)
+            parsed_params[key] = {
+                "type": arg_type,
+                "nargs": "+",
+                "default": default_value,
+                "help": name,
+            }
         elif isinstance(default_value, dict):
-            # Handle dictionaries by converting them to strings
-            yaml_string = yaml.dump(default_value)
-            parser.add_argument(f"--{key}", type=str,
-                                default=yaml_string, help=name)
+            # Handle dictionaries by directly using the value
+            parsed_params[key] = {
+                "type": type(default_value),
+                "default": default_value,
+                "help": name,
+            }
         else:
-            parser.add_argument(
-                f"--{key}", type=type(default_value), default=default_value, help=name)
+            parsed_params[key] = {
+                "type": arg_type,
+                "default": default_value,
+                "help": name,
+            }
+    return parsed_params
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Load the YAML configuration
+    config = load_yaml("parameters.yaml")
+
+    # Iterate through the config and add arguments to the parser
+    for section_name, section_data in config.items():
+        if section_name == "parameters":
+            parameters = parse_parameters(section_data)
+            for key, value in parameters.items():
+                parser.add_argument(f"--{key}", **value)
+        else:
+            for key, value in section_data.items():
+                if "help" in value and "value" in value:
+                    name = value["help"]
+                    default_value = value["value"]
+                else:
+                    name = key.replace("_", " ").capitalize()
+                    default_value = value
+
+                arg_type = get_arg_type(default_value)
+
+                if isinstance(default_value, list):
+                    # Handle lists by specifying the type of the first element
+                    parser.add_argument(
+                        f"--{key}",
+                        type=arg_type,
+                        nargs="+",
+                        default=default_value,
+                        help=name,
+                    )
+                elif isinstance(default_value, dict):
+                    # Handle dictionaries by directly using the value
+                    parser.add_argument(
+                        f"--{key}",
+                        type=type(default_value),
+                        default=default_value,
+                        help=name,
+                    )
+                else:
+                    parser.add_argument(
+                        f"--{key}", type=arg_type, default=default_value, help=name
+                    )
 
     return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    print(args.train_params["seed"]["value"])
