fc0404ed45ce45aac1c9eda417016d790c36c9ec
pasqualedem <pasquale199876@hotmail.it>
2023-10-23 11:55:02 +0200

    implemented batch

--- label_anything/substitution.py
+++ label_anything/substitution.py
@@ -1,6 +1,10 @@
 import torch
 
 
+def cantor_function(x, y)
+    return 0.5 * (x + y) * (x + y + 1) + y
+
+
 def generate_points_from_errors(prediction, ground_truth, num_points):
     """
     Generates a point for each class that can be positive or negative depending on the error being false positive or false negative.
@@ -10,9 +14,9 @@ def generate_points_from_errors(prediction, ground_truth, num_points):
         num_points (int): The number of points to generate for each class
     """
     errors = ground_truth - prediction
-    coords = [torch.nonzero(errors[i]) for i in range(errors.shape[0])]
-    sampled_idxs = [torch.randint(0, coord_dim.shape[0], (num_points,)) for coord_dim in coords]
-    sampled_points = torch.cat([coords[dim][idx] for dim, idx in enumerate(sampled_idxs)])
-    sampled_points = torch.cat([torch.arange(errors.shape[0]).repeat_interleave(num_points).unsqueeze(0).T, sampled_points], dim=1)
-    labels = errors[sampled_points[:, 0], sampled_points[:, 1], sampled_points[:, 2]]
+    coords = torch.nonzero(errors)
+    _, counts = torch.unique(coords[:, 0:2], dim=0, return_counts=True, sorted=True)
+    sampled_idxs = torch.cat([torch.randint(0, x, (num_points,)) for x in counts]) + torch.cat([torch.tensor([0]), counts.cumsum(dim=0)])[:-1].repeat_interleave(num_points)
+    sampled_points = coords[sampled_idx]
+    labels = errors[sampled_points[:, 0], sampled_points[:, 1], sampled_points[:, 2], sampled_points[:, 3]]
     return sampled_points, labels
