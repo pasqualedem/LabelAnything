0dc665b5a6167aec6d8a62a609f59b155849d82e
Faiail <raffaele.scaringi20@gmail.com>
2024-01-17 16:46:40 +0100

    up

--- label_anything/data/coco.py
+++ label_anything/data/coco.py
@@ -699,6 +699,10 @@ class CocoLVISTestDataset(CocoLVISDataset):
 
     def __getitem__(self, item):
         data, data_key, gt = self._get_images_or_embeddings([self.images[self.image_ids[item]]])
+        print(gt)
+        print(type(gt))
+        print(len(gt))
+        exit()
         gt = torch.as_tensor(gt)
         dim = torch.as_tensor(gt.size())
         data_dict = {
