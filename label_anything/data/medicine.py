574099e552f64aac55a7da0d1ecb9595964f9ba7
Carpi <colonnamanu98@gmail.com>
2024-02-07 18:32:58 +0100

    Add image annotation functionality

--- label_anything/data/medicine.py
+++ label_anything/data/medicine.py
@@ -1,11 +1,14 @@
+import json
 import os
 import glob
-import cv2
-from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader
 import numpy as np
+from PIL import Image
+from pycocotools import mask as mask_utils
 import pandas as pd
 from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+data_dict = {"images": [], "annotations": [], "categories": []}
 
 
 def download_and_extract_dataset(
@@ -19,74 +22,86 @@ def download_and_extract_dataset(
     print("Downloaded and extracted dataset:", dataset_name)
 
 
-def generate_data_map(path):
+def generate_data(path):
     data_map = []
+    if not path.endswith("/"):
+        path += "/"
     for sub_dir_path in glob.glob(path + "*"):
         if os.path.isdir(sub_dir_path):
             dirname = sub_dir_path.split("/")[-1]
             for filename in os.listdir(sub_dir_path):
                 image_path = sub_dir_path + "/" + filename
                 data_map.extend([dirname, image_path])
-    return data_map
+
+    images = sorted([p for p in data_map[1::2] if "mask" not in p])
+    masks = sorted([p for p in data_map[1::2] if "mask" in p])
+    return images, masks
 
 
-def generate_df(path):
-    data_map = generate_data_map(path)
-    df = pd.DataFrame({"dirname": data_map[::2], "path": data_map[1::2]})
-    BASE_LEN = len(os.path.join(path, "TCGA_DU_6404_19850629/TCGA_DU_6404_19850629_"))
-    END_IMG_LEN = 4
-    END_MASK_LEN = 9
+def generate_mask_bbox(mask_file) -> (dict, list):
+    mask_array = np.array(Image.open(mask_file))
+    rle_masks = {}
+    for key, value in masks.items():
+        rle = mask_utils.encode(np.asfortranarray(value.astype(np.uint8)))
+        rle["counts"] = rle["counts"].decode("utf-8")  # Convert bytes to string
+        rle_masks[key] = rle
 
-    df_imgs = df[~df["path"].str.contains("mask")]
-    df_masks = df[df["path"].str.contains("mask")]
+    # Get the non-zero pixel coordinates
+    y_indices, x_indices = np.nonzero(mask_array)
 
-    imgs = sorted(df_imgs["path"].values, key=lambda x: int(x[BASE_LEN:-END_IMG_LEN]))
-    masks = sorted(
-        df_masks["path"].values, key=lambda x: int(x[BASE_LEN:-END_MASK_LEN])
-    )
+    # Check if there are any non-zero pixels
+    if y_indices.size > 0 and x_indices.size > 0:
+        # If there are non-zero pixels, calculate the bounding box
+        x_min, x_max = np.min(x_indices), np.max(x_indices)
+        y_min, y_max = np.min(y_indices), np.max(y_indices)
+        bbox = [x_min, y_min, x_max - x_min, y_max - y_min]
+    else:
+        # If there are no non-zero pixels, return an empty bounding box
+        bbox = None
 
-    df = pd.DataFrame(
-        {"patient": df_imgs.dirname.values, "image_path": imgs, "mask_path": masks}
-    )
-
-    df["diagnosis"] = df["mask_path"].apply(lambda m: diagnose(m))
-
-    if not os.path.exists("data/processed"):
-        os.makedirs("data/processed")
-
-    try:
-        df.to_csv("data/processed/brain_mri_df.csv", index=False)
-    except Exception as e:
-        print(e)
+    return rle_masks, bbox
 
 
-def diagnose(mask_path):
-    value = np.max(cv2.imread(mask_path))
-    return 1 if value > 0 else 0
+def generate_annotations(images, masks, annotations):
+    annotations_images = []
+    annotations_segmentations = []
 
+    for idx, (image, mask) in enumerate(zip(images, masks)):
+        image_name = image.split("/")[-1].split(".")[0]
+        annotations_images.append(
+            {
+                "file_name": image_name,
+                "url": image,
+                "height": Image.open(image).size[1],
+                "width": Image.open(image).size[0],
+                "id": idx,
+            }
+        )
 
-class BrainMriDataset(Dataset):
-    def __init__(self, df, transforms=None):
-        self.df = pd.read_csv(df, index_col=False)
-        self.transforms = transforms
+        rle, bbox = generate_mask_bbox(mask)
+        if bbox is None:
+            category_id = 0
+        else:
+            category_id = 1
+        annotations_segmentations.append(
+            {
+                "segmentation": rle["counts"],
+                "area": int(mask_utils.area(rle)),
+                "image_id": idx,
+                "bbox": bbox,
+                "category_id": category_id,
+                "id": idx,
+            }
+        )
 
-    def __len__(self):
-        return len(self.df)
+    annotations["images"] = annotations_images
+    annotations["annotations"] = annotations_segmentations
+    annotations["categories"] = [
+        {"id": 0, "name": "no_tumor"},
+        {"id": 1, "name": "tumor"},
+    ]
 
-    def __getitem__(self, idx):
-        image = cv2.imread(self.df.iloc[idx, 1])
-        mask = cv2.imread(self.df.iloc[idx, 2], 0)
-
-        if self.transforms is not None:
-            image = self.transforms(image)
-            mask = self.transforms(mask)
-
-        data_dict = {
-            "image": image,
-            "mask": mask,
-        }
-
-        return data_dict
+    return annotations
 
 
 if __name__ == "__main__":
@@ -96,13 +111,8 @@ if __name__ == "__main__":
         download_and_extract_dataset()  # all parameters are set by default
 
     path = "data/raw/lgg-mri-segmentation/kaggle_3m/"
-    generate_df(path)
-    dataset = BrainMriDataset(
-        "data/processed/brain_mri_df.csv",
-        transforms=transforms.ToTensor(),
-    )
-
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-    i = next(iter(dataloader))
-    print(f'image: {i["image"].shape}, mask: {i["mask"].shape}')
-    # image: torch.Size([4, 3, 256, 256]), mask: torch.Size([4, 1, 256, 256])
+    images, masks = generate_data(path)
+    data_dict = generate_annotations(images, masks, data_dict)
+    print(data_dict)
+    with open("data/annotations/brain_mri.json", "w") as f:
+        json.dump(data_dict, f)
