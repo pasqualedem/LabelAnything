d50f7c3f48ba40679d03a19824afc9b8ccc7a566
Faiail <raffaele.scaringi20@gmail.com>
2024-02-09 18:05:21 +0100

    fix batched input dimensions

--- label_anything/data/prompt_encoder_dataset.py
+++ label_anything/data/prompt_encoder_dataset.py
@@ -75,7 +75,6 @@ class PromptEncoderDataset(CocoLVISDataset):
         points, flag_points = self.annotations_to_tensor(
             points, img_sizes, PromptType.POINT
         )
-        dims = torch.tensor(img_sizes)
 
         # load clip embeddings
         clip_embeddings = torch.stack([self._load_clip_embeddings(img_id) for img_id in img_ids])
@@ -87,7 +86,6 @@ class PromptEncoderDataset(CocoLVISDataset):
             BatchKeys.FLAG_POINTS: flag_points,
             BatchKeys.PROMPT_BBOXES: bboxes,
             BatchKeys.FLAG_BBOXES: flag_bboxes,
-            BatchKeys.DIMS: dims,
             BatchKeys.CLIP_EMBEDDINGS: clip_embeddings
         }
 
@@ -98,7 +96,7 @@ class PromptEncoderDataset(CocoLVISDataset):
 def collate_fn(batched_input: list[dict[BatchKeys, torch.Tensor]]) -> dict[BatchKeys, torch.Tensor]:
     # collate images or embeddings
     image_key = BatchKeys.IMAGES if BatchKeys.IMAGES in batched_input[0].keys() else BatchKeys.EMBEDDINGS
-    images = torch.stack([x[image_key] for x in batched_input])
+    images = torch.stack([x[image_key] for x in batched_input], dim=1)
 
     # collate masks
     masks = [x[BatchKeys.PROMPT_MASKS] for x in batched_input]
@@ -134,16 +132,13 @@ def collate_fn(batched_input: list[dict[BatchKeys, torch.Tensor]]) -> dict[Batch
     # collate clip embeddings
     clip_embeddings = torch.stack([x[BatchKeys.CLIP_EMBEDDINGS] for x in batched_input])
 
-    # collate dims
-    dims = torch.stack([x[BatchKeys.DIMS] for x in batched_input])
     return {
         image_key: images,
-        BatchKeys.PROMPT_MASKS: masks.unsqueeze(dim=0),
-        BatchKeys.FLAG_MASKS: flag_masks.unsqueeze(dim=0),
-        BatchKeys.PROMPT_BBOXES: bboxes.unsqueeze(dim=0),
-        BatchKeys.FLAG_BBOXES: flag_bboxes.unsqueeze(dim=0),
-        BatchKeys.PROMPT_POINTS: points.unsqueeze(dim=0),
-        BatchKeys.FLAG_POINTS: flag_points.unsqueeze(dim=0),
-        BatchKeys.DIMS: dims.unsqueeze(dim=0),
+        BatchKeys.PROMPT_MASKS: masks,
+        BatchKeys.FLAG_MASKS: flag_masks,
+        BatchKeys.PROMPT_BBOXES: bboxes,
+        BatchKeys.FLAG_BBOXES: flag_bboxes,
+        BatchKeys.PROMPT_POINTS: points,
+        BatchKeys.FLAG_POINTS: flag_points,
         BatchKeys.CLIP_EMBEDDINGS: clip_embeddings,
     }
