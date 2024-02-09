85240ec12a9d9f93b5bb783ee0eb0926ac2c0add
Faiail <raffaele.scaringi20@gmail.com>
2024-02-09 17:28:12 +0100

    fix bug image dimension

--- label_anything/data/prompt_encoder_dataset.py
+++ label_anything/data/prompt_encoder_dataset.py
@@ -137,7 +137,7 @@ def collate_fn(batched_input: list[dict[BatchKeys, torch.Tensor]]) -> dict[Batch
     # collate dims
     dims = torch.stack([x[BatchKeys.DIMS] for x in batched_input])
     return {
-        image_key: images.unsqueeze(dim=0),
+        image_key: images,
         BatchKeys.PROMPT_MASKS: masks.unsqueeze(dim=0),
         BatchKeys.FLAG_MASKS: flag_masks.unsqueeze(dim=0),
         BatchKeys.PROMPT_BBOXES: bboxes.unsqueeze(dim=0),
