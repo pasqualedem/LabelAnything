f8c4e20be9a4b533ecead0f2056fb1fc775d0f68
Faiail <raffaele.scaringi20@gmail.com>
2024-02-10 11:11:32 +0100

    up

--- label_anything/loss/symmetric.py
+++ label_anything/loss/symmetric.py
@@ -15,7 +15,7 @@ class SymmetricLoss(torch.nn.Module):
     def forward(self, input1, input2, labels):
         input1 = normalize(input1, p=2, dim=1) if self.norm else input1
         input2 = normalize(input2, p=2, dim=1) if self.norm else input2
-        logits = torch.mm(input1, input2.t()) * self.t
+        logits = torch.mm(input1, input2.t()) #* self.t
         loss1 = cross_entropy(logits, labels)
         loss2 = cross_entropy(logits.t(), labels.t())
         return (loss1 + loss2) / 2
