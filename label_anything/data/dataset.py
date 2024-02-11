Fix noclass_embeddings in embedding calculation

label_anything/data/dataset.py
@@ -179,6 +179,11 @@ class LabelAnythingDataset(Dataset):
         points = torch.stack([x[0] for x in points_flags])
         flag_points = torch.stack([x[1] for x in points_flags])
 
+        # flag examples
+        flag_examples = torch.stack(
+            [utils.collate_example_flags(x["flag_examples"], max_classes) for x in batched_input]
+        )
+
         # aux gts
         classes = [x["classes"] for x in batched_input]
 
@@ -206,6 +211,7 @@ class LabelAnythingDataset(Dataset):
             "flag_bboxes": flag_bboxes,
             "prompt_masks": masks,
             "flag_masks": flag_masks,
+            "flag_examples": flag_examples,
             "dims": dims,
             "classes": classes,
             "image_ids": image_ids,
@@ -249,12 +255,12 @@ def get_batch_metadata(
         val for tup in zip(*[batch_sizes for i in range(num_processes)]) for val in tup
     ]
     examples_nums = [
-        val for tup in zip(*[examples_nums for i in range(num_processes)])
+        val
+        for tup in zip(*[examples_nums for i in range(num_processes)])
         for val in tup
     ]
     prompt_types = [
-        val for tup in zip(*[prompt_types for i in range(num_processes)])
-        for val in tup
+        val for tup in zip(*[prompt_types for i in range(num_processes)]) for val in tup
     ]
     batch_metadata = {
         utils.BatchMetadataKeys.NUM_EXAMPLES: examples_nums,

label_anything/loss/__init__.py
@@ -78,9 +78,7 @@ class LabelAnythingLoss(nn.Module):
             self.weights[k]
             * loss(
                 result[ResultDict.EXAMPLES_CLASS_EMBS],
-                result[BatchKeys.FLAG_MASKS],
-                result[BatchKeys.FLAG_POINTS],
-                result[BatchKeys.FLAG_BBOXES],
+                result[BatchKeys.FLAG_EXAMPLES],
             )
             for k, loss in self.prompt_components.items()
         )

label_anything/models/prompt_encoder.py
@@ -504,6 +504,7 @@ class PromptImageEncoder(PromptEncoder):
         points: Optional[Tuple[torch.Tensor, torch.Tensor]],
         boxes: Optional[torch.Tensor],
         masks: Optional[torch.Tensor],
+        flag_examples: Optional[torch.Tensor],
         chunk_size=None,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
@@ -515,6 +516,7 @@ class PromptImageEncoder(PromptEncoder):
             and labels to embed (B, M, C, 2)
           boxes (torch.Tensor or none): boxes to embed (B, M, C, 2, 2)
           masks (torch.Tensor or none): masks to embed (B, M, C, H, W)
+          flag_examples (torch.Tensor or none): flags to indicate which examples (B, M, C)
 
         Returns:
           torch.Tensor: sparse embeddings for the points and boxes, with shape
@@ -558,8 +560,9 @@ class PromptImageEncoder(PromptEncoder):
             src = self.class_attention(src)
             src = rearrange(src, "(b m) c d -> b m c d", m=m)
 
-        # Average over examples
-        class_embeddings = torch.mean(src, dim=1)  # (B, C, D)
+        # Average over examples removing padding embeddings
+        masked_src = src * flag_examples.unsqueeze(-1)
+        class_embeddings = masked_src.sum(dim=1) / flag_examples.sum(dim=1).unsqueeze(-1)
         return {
             ResultDict.CLASS_EMBS: class_embeddings,
             ResultDict.EXAMPLES_CLASS_EMBS: src,