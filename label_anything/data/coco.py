82addaa5077f438a89ab3b884a72fe1b8c2a53a3
pasqualedem <pasquale199876@hotmail.it>
2024-02-12 15:02:55 +0100

    fix choice problem

--- label_anything/data/coco.py
+++ label_anything/data/coco.py
@@ -329,7 +329,7 @@ class CocoLVISDataset(Dataset):
                 if n_ann > self.max_points_annotations:
                     prompt_types = [PromptType.MASK] * n_ann
                 else:
-                    prompt_types = np.random.choice(possible_prompt_types, size=n_ann)
+                    prompt_types = random.choices(possible_prompt_types, k=n_ann)
 
                 for ann, prompt_type in zip(
                     self.img2cat_annotations[img_id][cat_id], prompt_types
