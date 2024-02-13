7ad2a3d749249535ae4d30af3a0dc4ceebf95ac9
pasqualedem <pasquale199876@hotmail.it>
2024-02-13 11:28:00 +0100

    updated cli

--- label_anything/experiment/run.py
+++ label_anything/experiment/run.py
@@ -617,6 +617,7 @@ class ParallelRun:
     slurm_script_first_parameter = "--parameters="
     slurm_outfolder = "out"
     out_extension = "out"
+    param_extension = "yaml"
     slurm_stderr = "-e"
     slurm_stdout = "-o"
 
@@ -627,14 +628,16 @@ class ParallelRun:
             sys.path.extend(".")
 
     def launch(self, only_create=False):
-        tmp_parameters_file = tempfile.NamedTemporaryFile(delete=False)
-        write_yaml(self.params, tmp_parameters_file.name)
-        tmp_parameters_file.close()
         subfolder = f"{get_timestamp()}_{self.exp_uuid}_{self.params['experiment']['group']}"
         out_folder = os.path.join(self.slurm_outfolder, subfolder)
         os.makedirs(out_folder, exist_ok=True)
-        out_file = f"run_{str(uuid.uuid4())[:8]}.{self.out_extension}"
+        
+        run_uuid = str(uuid.uuid4())[:8]
+        out_file = f"{run_uuid}.{self.out_extension}"
         out_file = os.path.join(out_folder, out_file)
+        param_file = f"{run_uuid}.{self.param_extension}"
+        param_file = os.path.join(out_folder, param_file)
+        write_yaml(self.params, param_file)
         command = [
             self.slurm_command,
             self.slurm_stdout,
