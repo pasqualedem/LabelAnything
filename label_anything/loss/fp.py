eeffd4f7e68c1ec5613cc64c736dfcbad3908d79
pasqualedem <pasquale199876@hotmail.it>
2024-03-02 22:55:09 +0100

    Fix fp loss

--- label_anything/loss/fp.py
+++ label_anything/loss/fp.py
@@ -7,20 +7,30 @@ from .utils import get_reduction
 
 
 class FalsePositiveLoss(Module):
-    def __init__(self, **kwargs):
+    def __init__(self, ignore_index=-100, **kwargs):
         super().__init__()
         self.eps = 1e-6
+        self.ignore_index = ignore_index
 
     def __call__(self, x, target, weight_matrix=None, **kwargs):
-        included_classes = torch.stack([item.unique() for item in target])
+        mask = (target != self.ignore_index)
+        valid_elements = mask.sum()
+        
+        full_target = target.clone()
+        full_target[~mask] = 0
+        
+        included_classes = [item.unique() for item in full_target]
         not_included_classes = torch.ones(x.shape[:2], device=x.device)
-        not_included_classes.scatter_(1, included_classes, 0)
+        for i, item in enumerate(included_classes):
+            not_included_classes[i].scatter_(0, item, 0)
         not_included_classes = rearrange(not_included_classes, "b c -> b c () ()")
+        mask = rearrange(mask, "b h w -> b () h w")
 
         soft_x = F.softmax(x, dim=1)
-        false_positive_loss = soft_x * not_included_classes
+        false_positive_loss = soft_x * not_included_classes * mask
         false_positive_loss = false_positive_loss.sum(dim=1) / (
             not_included_classes.sum(dim=1) + self.eps
         )
-        false_positive_loss = false_positive_loss.mean()
+        false_positive_loss = false_positive_loss.sum() / valid_elements
+        print(f"False positive loss: {false_positive_loss}")
         return false_positive_loss
