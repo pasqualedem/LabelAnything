0b067e0dd41df7c1ebff41d3e62711f5f0887e3c
pasqualedem <pasquale199876@hotmail.it>
2024-03-09 18:10:58 +0100

    Change default run id

--- label_anything/demo/streamlit.py
+++ label_anything/demo/streamlit.py
@@ -557,7 +557,7 @@ def main():
     accelerator = Accelerator()
     with st.sidebar:
         # load model
-        run_id = st.text_input("Run ID", "n9623ye5")
+        run_id = st.text_input("Run ID", "3ndl7had")
         model = load_model(accelerator, run_id).model  # WrapperModule
         image_encoder = st.selectbox("Image Encoder", options=["vit_sam_b"])
         image_encoder = load_image_encoder(image_encoder)
